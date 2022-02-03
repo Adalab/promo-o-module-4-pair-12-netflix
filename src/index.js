088ea00d6f02ea9660ef5a6965b0fcba698e1e54
Claudia <claudia.berrocalgomez@gmail.com>
2022-02-03 20:50:28 +0100

    add path to public folder

--- src/index.js
+++ src/index.js
@@ -22,16 +22,5 @@ server.get("/movies", (req, res) => {
 });
 
 // config express static server
-const staticServerPath = "./public-react";
+const staticServerPath = "./src/public-react";
 server.use(express.static(staticServerPath));
-
-// // not found error
-// server.get("*", (req, res) => {
-//   // relative to this directory
-//   const notFoundFileRelativePath = "./public-react/404-not-found.html";
-//   const notFoundFileAbsolutePath = path.join(
-//     __dirname,
-//     notFoundFileRelativePath
-//   );
-//   res.status(404).sendFile(notFoundFileAbsolutePath);
-// });

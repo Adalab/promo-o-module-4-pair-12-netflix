8a3e45771da71e723b288a55d845d1f7b06e18aa
Claudia <claudia.berrocalgomez@gmail.com>
2022-02-03 11:03:47 +0100

    add public folder (to complete)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const dataMovies = require('./data/movies.json');
+const express = require("express");
+const cors = require("cors");
+const dataMovies = require("./data/movies.json");
+const path = require("path");
 
 // create and config server
 const server = express();
@@ -12,10 +13,25 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
-server.get('/movies', (req, res) => {
+server.get("/movies", (req, res) => {
   const data = {
     success: true,
-    movies: dataMovies
-  }
+    movies: dataMovies,
+  };
   res.json(data);
-})
+});
+
+// config express static server
+const staticServerPath = "./public-react";
+server.use(express.static(staticServerPath));
+
+// // not found error
+// server.get("*", (req, res) => {
+//   // relative to this directory
+//   const notFoundFileRelativePath = "./public-react/404-not-found.html";
+//   const notFoundFileAbsolutePath = path.join(
+//     __dirname,
+//     notFoundFileRelativePath
+//   );
+//   res.status(404).sendFile(notFoundFileAbsolutePath);
+// });
